d5daa0331522ccf15f543efe75f8d608e1b6d87e
Asha <shree.asha10@gmail.com>
2018-11-08 18:56:15 +0530

    Transpiling - with babel - .babelrc file - use babel-node instead node to run the scripts which in ES6

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,2 +1,4 @@
-var chalk = require('chalk');
+// var chalk = require('chalk');
+// The above one is ES5 code, replacing with ES6 import to check transpiling vial babel
+import chalk from "chalk";
 console.log(chalk.green('Starting app in dev mode...'));
