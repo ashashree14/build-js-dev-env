8457d512e81e8cc4a3b9713d5e3a77d9ed1aa851
Asha <shree.asha10@gmail.com>
2018-11-18 19:18:56 +0530

    Linting: configure linting with .eslintrc.json and use eslint-watch to run linting on every save

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 // var chalk = require('chalk');
 // The above one is ES5 code, replacing with ES6 import to check transpiling vial babel
 import chalk from "chalk";
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
